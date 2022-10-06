a330023e94fc8fc071777b622e425304556ed289
Matt Bremmers <bremmdev@gmail.com>
2022-10-06 23:26:41 +0200

    add image gallery

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,15 +5,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-gray-50">
       <header className="border-b-2 border-rose-300 bg-white">
-        <div className="flex items-center max-w-6xl mx-auto px-8 py-4">
+        <div className="flex items-center max-w-6xl mx-auto px-8 py-4 sm:px-12">
           <Image src={logo} alt="logo" width={42} height={42} />
-          <h1 className="font-handwriting text-rose-300 text-3xl ml-6">
+          <h1 className="font-handwriting text-rose-300 text-3xl tracking-wider ml-6">
             Kattenparadijs
           </h1>
         </div>
       </header>
 
-      <main className="max-w-6xl mx-auto p-8">{children}</main>
+      <main className="max-w-6xl mx-auto py-12 px-8 sm:px-12">{children}</main>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,75 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { GetStaticProps, NextPage } from "next";
+import Image from "next/image";
+import { createClient } from "@supabase/supabase-js";
+import { useState } from "react";
+import probe from "probe-image-size";
 
-const Home: NextPage = () => {
-  return (
-      <div>Gallery</div>
-  
-  )
+interface ImageObject {
+  url: string;
+  width: number;
+  height: number;
 }
 
-export default Home
+const Home: NextPage<{ imageObjs: ImageObject[] }> = ({ imageObjs }) => {
+  return (
+    <div className="columns-2 space-y-8 gap-8 sm:gap-10 md:columns-3">
+      {imageObjs?.map((img, idx) => (
+        <div className="mb-8" key={idx}>
+          <Image
+            src={img.url}
+            width={img.width}
+            height={img.height}
+            alt="kat"
+            className="rounded-xl"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
+
+async function createImgObj(url: string) {
+  //get the width and height from the img url
+  let result = await probe(url);
+  return {
+    url: result.url,
+    width: result.width,
+    height: result.height,
+  };
+}
+
+export async function getStaticProps() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+  const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
+  let imageObjs: ImageObject[] = [];
+
+  //list of all the image in the bucker
+  const { data: images, error } = await supabase.storage.from("images").list();
+
+  let imageUrls: string[] = [];
+
+  if (images) {
+    const imageNames = images.map((item) => item.name);
+    imageUrls = imageNames.map((imageName) => {
+      const { data } = supabase.storage.from("images").getPublicUrl(imageName);
+      return data!.publicURL;
+    });
+
+    //add the height and width to each url
+    imageObjs = await Promise.all(
+      imageUrls.map(async (url) => {
+        return await createImgObj(url);
+      })
+    );
+  }
+
+  return {
+    props: {
+      imageObjs,
+    },
+  };
+}
