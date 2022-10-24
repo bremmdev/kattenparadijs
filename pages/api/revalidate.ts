d2c465bef74d2c1c64e6683acbbc03fd2a3894db
Matt Bremmers <bremmdev@gmail.com>
2022-10-24 23:12:03 +0200

    test delete and update webhook

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,9 +4,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    return res.status(401).json({ message: "Must be a POST request" });
-  }
+  // if (req.method !== "POST" || '') {
+  //   return res.status(401).json({ message: "Must be a POST request" });
+  // }
 
   // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
   //   return res.status(401).json({ message: "Invalid token" });
