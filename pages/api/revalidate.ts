aadad057b846c60a6c94b2f7338f753658baa874
Matt Bremmers <bremmdev@gmail.com>
2022-10-24 23:55:23 +0200

    add secret for webhook v2

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,16 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { isValidSignature } from "@sanity/webhook";
+import { isValidRequest } from "@sanity/webhook"
 
-const secret = process.env.SANITY_WEBHOOK_SECRET;
+const secret = process.env.SANITY_WEBHOOK_SECRET || '';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-    return res.status(401).json({ message: "Invalid token" });
+  if (!isValidRequest(req, secret)) {
+    res.status(401).json({ message: "Invalid signature" })
+    return
   }
-
   try {
     const {
       body: { _type, cat },
