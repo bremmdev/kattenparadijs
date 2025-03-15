cae0f9e27442174652f8ba564a56c10fc082dd96
bremmdev <bremmdev@gmail.com>
2025-03-15 15:30:10 +0100

    add passedaway status to avatars

--- components/Cat/Avatars.tsx
+++ components/Cat/Avatars.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
+import PassingIcon from "./PassingIcon";
 
 const avatars = [
   { src: "/avatar/daantje.svg", path: "/daantje" },
-  { src: "/avatar/flynn.svg", path: "/flynn" },
-  { src: "/avatar/moos.svg", path: "/moos" },
+  { src: "/avatar/flynn.svg", path: "/flynn", passedAway: true },
+  { src: "/avatar/moos.svg", path: "/moos", passedAway: true },
   { src: "/avatar/norris.svg", path: "/norris" },
   { src: "/avatar/cats.svg", path: "/all" },
   { src: "/avatar/cats-videos.svg", path: "/videos" },
@@ -13,17 +14,20 @@ const avatars = [
 const Avatars = () => {
   return (
     <div className="flex gap-3 justify-center my-2 sm:my-0 sm:ml-auto">
-      {avatars.map((avatar, idx) => (
-        <Link key={idx} href={avatar.path}>
+      {avatars.map((avatar, idx) => {
+        return (
+          <Link key={idx} href={avatar.path} className="relative">
+            {avatar.passedAway && <PassingIcon className="absolute right-1 z-10"/>}
             <Image
-              className="transition-all duration-300 cursor-pointer hover:scale-110 hover:brightness-105"
+              className={`${avatar.passedAway ? 'opacity-50' : ''} transition-all duration-300 cursor-pointer hover:scale-110 hover:brightness-105`}
               src={avatar.src}
               alt="cat avatar"
               width="42"
               height="42"
             />
-        </Link>
-      ))}
+          </Link>
+        );
+      })}
     </div>
   );
 };
