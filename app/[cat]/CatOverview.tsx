d3dc4bae9f04ac65ac566d431e93eaff49bfb75b
bremmdev <bremmdev@gmail.com>
2025-08-28 15:32:21 +0200

    add norris banner

--- app/[cat]/CatOverview.tsx
+++ app/[cat]/CatOverview.tsx
@@ -7,6 +7,8 @@ import useWindowSize from "@/hooks/useWindowSize";
 import Confetti from "react-confetti";
 import Bio from "@/components/Cat/Bio";
 import { checkBirthday } from "@/utils/checkBirthday";
+import NorrisBanner from "@/public/norris_banner.webp";
+import Image from "next/image";
 
 const CatOverview = ({
   cat,
@@ -32,6 +34,14 @@ const CatOverview = ({
     <>
       {showConfetti && <Confetti width={windowWidth} height={windowHeight} />}
       {cat && <Bio cat={cat} key={cat.name} />}
+      {cat?.name === "norris" && (
+        <Image
+          width={576}
+          className="w-full sm:w-xl rounded-lg block mx-auto my-8 sm:-mt-4"
+          src={NorrisBanner}
+          alt="Norris banner"
+        />
+      )}
       <Gallery cat={cat} isDetail={isDetail} />
     </>
   );
