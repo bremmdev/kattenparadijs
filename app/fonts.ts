527ae45998939745dd8095a0b03046c5a93ab127
bremmdev <bremmdev@gmail.com>
2025-09-13 14:27:28 +0200

    add display swap to custom fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,8 +3,10 @@ import { Poppins, Dancing_Script } from "next/font/google";
 export const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700"],
+  display: "swap",
 });
 
 export const dancing_script = Dancing_Script({
   subsets: ["latin"],
+  display: "swap",
 });
