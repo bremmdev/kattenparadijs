2158ff87f5fb9541937a477e6ab5c0b47b3c050f
bremmdev <bremmdev@gmail.com>
2025-11-28 17:45:07 +0100

    add csp

--- next.config.js
+++ next.config.js
@@ -1,7 +1,38 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-inline'; 
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://cdn.sanity.io;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
+
 module.exports = {
   experimental: {
     viewTransition: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader
+              .replace(/\n/g, " ")
+              .replace(/\s{2,}/g, " ")
+              .trim(),
+          },
+        ],
+      },
+    ];
   },
   reactStrictMode: true,
   images: {
