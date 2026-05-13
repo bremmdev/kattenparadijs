4bbc93e23811d6520b7f9568df5ebd186c3f004c
bremmdev <bremmdev@gmail.com>
2026-05-13 20:19:49 +0200

    revert back to inference api

--- search/vectorize-utils.ts
+++ search/vectorize-utils.ts
@@ -1,11 +1,4 @@
-import OpenAI from "openai";
-
 const INDEX_NAME = "cat-photos";
-
-const openai = new OpenAI({
-    apiKey: process.env.AZURE_AI_KEY!,
-    baseURL: process.env.AZURE_AI_ENDPOINT!,
-});
 
 export async function vectorizeImage(
     imageUrl: string
@@ -19,13 +12,28 @@ export async function vectorizeImage(
     const dataUrl = `data:${contentType};base64,${base64}`;
 
     try {
-        const embedding = await openai.embeddings.create({
-            model: "embed-v-4-0",
-            input: [dataUrl],
-            dimensions: 1024,
-        });
+        const res = await fetch(
+            `${process.env.AZURE_AI_ENDPOINT}/images/embeddings?api-version=2024-05-01-preview`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "api-key": process.env.AZURE_AI_KEY!,
+                },
+                body: JSON.stringify({
+                    model: "embed-v-4-0",
+                    input: [{ image: dataUrl }],
+                    dimensions: 1024,
+                }),
+            }
+        );
 
-        return embedding.data[0].embedding;
+        if (!res.ok) {
+            throw new Error("Image embedding error");
+        }
+
+        const data = await res.json();
+        return data.data[0].embedding;
     } catch (error) {
         console.error(error);
         throw new Error("Failed to vectorize image");
